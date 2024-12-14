29e5b8f5694766c63caa7c1d5fb169298018909e
Neo Tlaletsi <ntlaletsi86@gmail.com>
2024-12-15 00:16:13 +0200

    Tailor # Breaking changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -131,3 +131,8 @@ sleep 600
 function_connect_to_crossplane_providerconfigs
 function_setup_localstack
 function_health_check_crossplane
+sleep 120
+k apply -f environments.batch.blanketops.co.za.yaml
+k apply -f argocd/environments/microservice/dev .
+argocd admin initial-password -n argocd
+kubectl port-forward svc/argocd-server -n argocd 8081:443
