36d90ad7d0ba466ac2d3e1c0dae62e075d742a0a
Neo Tlaletsi <ntlaletsi86@gmail.com>
2024-12-14 21:09:53 +0200

    Tailor # aws ecs cleanup

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 function_uninstall(){
+
+   echo "Deleting all AWS ECS Resources---------------------------"
+   echo "---------------------------------------------------------"
+   kubectl delete capacityprovider.ecs.aws.upbound.io --all 
+   kubectl delete cluster.ecs.aws.upbound.io --all 
+   kubectl delete clustercapacityproviders.ecs.aws.upbound.io --all 
+   kubectl delete service.ecs.aws.upbound.io --all 
+   kubectl delete taskdefinition.ecs.aws.upbound.io --all 
+   echo "---------------------------------------------------------"
+
    echo "---------------------------------------------------------"
    echo "Deleting Resources"
    echo "---------------------------------------------------------"
+   kubectl delete 
    kubectl delete namespaces --all --force
    echo "---------------------------------------------------------"
    clear
