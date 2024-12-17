f385cd03d055dad91b9ece2f8ce1622f97319c44
Neo Tlaletsi <ntlaletsi86@gmail.com>
2024-12-17 20:24:15 +0200

    Tailor #, debt, api

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -2,6 +2,16 @@
 
 function_uninstall(){
 
+   echo "---------------------------------------------------------"
+   echo "Deleting all Crossplane Resources---------------------------"
+   echo "---------------------------------------------------------"
+   kubectl patch crd/environments.batch.blanketops.co.za -p '{"metadata":{"finalizers":[]}}' --type=merge
+   kubectl delete environment --all
+   kubectl delete composition --all
+   kubectl delete compositeresourcedefinition --all
+   echo "---------------------------------------------------------"
+
+
    echo "Deleting all ArgoCD Resources---------------------------"
    echo "---------------------------------------------------------"
    kubectl patch crd/applications.applications.argocd.crossplane.io -p '{"metadata":{"finalizers":[]}}' --type=merge
@@ -16,36 +26,14 @@ function_uninstall(){
    kubectl patch crd/applicationsets.argoproj.io -p '{"metadata":{"finalizers":[]}}' --type=merge
    kubectl delete applicationsets.argoproj.io--all
 
-
    kubectl patch crd/appprojects.argoproj.io -p '{"metadata":{"finalizers":[]}}' --type=merge
    kubectl delete appprojects.argoproj.io --all
-
-   
- 
 
    kubectl patch crd/service.ecs.aws.upbound.io -p '{"metadata":{"finalizers":[]}}' --type=merge
    kubectl delete service.ecs.aws.upbound.io --all
 
    kubectl patch crd/taskdefinition.ecs.aws.upbound.io -p '{"metadata":{"finalizers":[]}}' --type=merge
    kubectl delete taskdefinition.ecs.aws.upbound.io --all
-
-   helm repo remove localstack-charts
-   helm repo remove crossplane-stable
-   echo "---------------------------------------------------------"
-   echo "Deleting all Crossplane Resources---------------------------"
-   echo "---------------------------------------------------------"
-   kubectl delete environment --all
-   kubectl delete composition --all
-   kubectl delete compositeresourcedefinition --all
-   echo "---------------------------------------------------------"
-
-   echo "---------------------------------------------------------"
-   echo "Deleting Resources"
-   echo "---------------------------------------------------------"
-   kubectl delete 
-   kubectl delete namespaces --all --force
-   echo "---------------------------------------------------------"
-   clear
 
    echo "Deleting all AWS ECS Resources---------------------------"
    echo "---------------------------------------------------------"
@@ -58,17 +46,18 @@ function_uninstall(){
    kubectl patch crd/clustercapacityproviders.ecs.aws.upbound.io -p '{"metadata":{"finalizers":[]}}' --type=merge
    kubectl delete clustercapacityproviders.ecs.aws.upbound.io --all
 
-   applications.applications.argocd.crossplane.io
-   applications.argoproj.io
-   applicationsets.applicationsets.argocd.crossplane.io
-   applicationsets.argoproj.io
-   appprojects.argoproj.io
-
    kubectl patch crd/service.ecs.aws.upbound.io -p '{"metadata":{"finalizers":[]}}' --type=merge
    kubectl delete service.ecs.aws.upbound.io --all
 
    kubectl patch crd/taskdefinition.ecs.aws.upbound.io -p '{"metadata":{"finalizers":[]}}' --type=merge
    kubectl delete taskdefinition.ecs.aws.upbound.io --all
+
+   echo "---------------------------------------------------------"
+   echo "Deleting Namespaces"
+   echo "---------------------------------------------------------" 
+   kubectl delete namespaces --all --force
+   echo "---------------------------------------------------------"
+   clear
 
    helm repo remove localstack-charts
    helm repo remove crossplane-stable
